5e25369240d553a0e012ec24f3e660ee3af977db
basantaisg <basantaparajuli.11@gmail.com>
2025-03-30 13:43:10 +0545

    setting up post service for  create, get and get by id!

--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreatePostDto } from './dto/create-post.dto';
 
 @Injectable()
-export class PostService {}
+export class PostService {
+  constructor(private prisma: PrismaService) {}
+
+  async createPost(createPostDto: CreatePostDto) {
+    return this.prisma.post.create({
+      data: createPostDto,
+    });
+  }
+
+  async getPosts() {
+    return this.prisma.post.findMany({ include: { User: true } });
+  }
+
+  async getPostById(id: string) {
+    return this.prisma.post.findUnique({
+      where: { id },
+      include: { User: true },
+    });
+  }
+}
