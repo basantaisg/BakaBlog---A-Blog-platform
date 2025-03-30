c2bdd85b7c8c732fe96169e6334d15483be1e478
basantaisg <basantaparajuli.11@gmail.com>
2025-03-30 13:29:32 +0545

    adding and setting up bcrypt js

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,7 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { createUserDto } from './dto/create-user.dto';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class UserService {
   constructor(private prisma: PrismaService) {}
+
+  async createUser(createUserDto: createUserDto) {
+    const { username, email, password } = createUserDto;
+    const hashedPassword = await bcrypt.hash(password, 10);
+  }
 }
