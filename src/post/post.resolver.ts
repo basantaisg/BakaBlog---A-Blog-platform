55d43c1bbc3dc61ab2aa3859873defd3fc4be77d
basantaisg <basantaparajuli.11@gmail.com>
2025-03-30 13:46:16 +0545

    setting up post resolver and wrapping up everything!

--- src/post/post.resolver.ts
+++ src/post/post.resolver.ts
@@ -1,9 +1,24 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { PostService } from './post.service';
+import { Post } from './model/post.model';
+import { CreatePostDto } from './dto/create-post.dto';
 
 @Resolver()
 export class PostResolver {
-  @Query(() => String)
-  helloPost() {
-    return 'Hello, Post!';
+  constructor(private postService: PostService) {}
+
+  @Query(() => [Post])
+  async getPosts() {
+    return this.postService.getPosts();
+  }
+
+  @Query(() => Post)
+  async getPostById(@Args('id') id: string) {
+    return this.postService.getPostById(id);
+  }
+
+  @Mutation(() => Post)
+  async createPost(@Args('input') input: CreatePostDto) {
+    return this.postService.createPost(input);
   }
 }
