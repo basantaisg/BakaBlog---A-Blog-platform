6ffb134c3d4e75e9ee99d0929f30f88a299d08c2
basantaisg <basantaparajuli.11@gmail.com>
2025-03-30 13:27:45 +0545

    setting up prismaService and module and implementing on everywhere!

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { PostModule } from './post/post.module';
 import { UserModule } from './user/user.module';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [
@@ -14,6 +15,7 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
     }),
     PostModule,
     UserModule,
+    PrismaModule,
   ],
   controllers: [AppController],
   providers: [AppService],
