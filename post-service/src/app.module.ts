15a96aabbdf6f4b5471f1a00f61a9dbe55c83c28
basantaisg <basantaparajuli.11@gmail.com>
2025-03-30 15:46:18 +0545

    changes in apollo driver!

--- post-service/src/app.module.ts
+++ post-service/src/app.module.ts
@@ -2,9 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { PostModule } from './post/post.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
 @Module({
-  imports: [PrismaModule, PostModule],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: 'src/graphql/schema.gql',
+    }),
+    PrismaModule,
+    PostModule,
+  ],
   providers: [AppService],
 })
 export class AppModule {}
