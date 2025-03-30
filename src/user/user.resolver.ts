06df68cc8c2f86555c2db8fe86689ded8bbdcd98
basantaisg <basantaparajuli.11@gmail.com>
2025-03-30 13:56:29 +0545

    creating update user and delete user functionality!

--- src/user/user.resolver.ts
+++ src/user/user.resolver.ts
@@ -2,6 +2,7 @@ import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { UserService } from './user.service';
 import { User } from './model/user.model';
 import { createUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Resolver()
 export class UserResolver {
@@ -24,5 +25,18 @@ export class UserResolver {
   @Mutation(() => User)
   async createUser(@Args('input') input: createUserDto) {
     return this.userService.createUser(input);
+  }
+
+  // update User!
+
+  @Mutation(() => User)
+  async updateUser(@Args('updateInput') updateInput: UpdateUserDto) {
+    return this.userService.updateUser(updateInput.id, updateInput);
+  }
+
+  // delete user!
+  @Mutation(() => User)
+  async deleteUser(@Args('id') id: string) {
+    return this.userService.deleteUser(id);
   }
 }
