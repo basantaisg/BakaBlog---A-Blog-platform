1ed0d66a0516d79f740e786434df625568f008b9
basantaisg <basantaparajuli.11@gmail.com>
2025-03-30 12:59:14 +0545

    apollodriverconfig updat e

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,17 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql';
-import { join } from 'path';
-import { ApolloServer } from '@apollo/server';
 import { PostModule } from './post/post.module';
 import { UserModule } from './user/user.module';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
 @Module({
   imports: [
-    GraphQLModule.forRoot({
-      driver: ApolloServer,
-      autoschemaFile: join(process.cwd(), 'src/graphql/schema.gql'),
-      playground: true,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: 'src/graphql/schema.gql',
     }),
     PostModule,
     UserModule,
